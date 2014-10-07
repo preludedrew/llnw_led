5c24a7967bacaa3d5ab460c70ccb751c976143cd
Andrew Boren <andrew.boren@gmail.com>
2014-10-07 16:09:19 -0700

    Cleanup a bit.

--- minimal-example.cc
+++ minimal-example.cc
@@ -9,6 +9,7 @@
 #include <math.h>
 
 #include <assert.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <algorithm>
@@ -26,299 +27,158 @@ class NumberCanvas : public ThreadedCanvasManipulator {
 public:
     int maxDays;
     int daysSince;
-    NumberCanvas(Canvas *m, int max, int since) : ThreadedCanvasManipulator(m), max_(max), since_(since) {
+    NumberCanvas(Canvas *m, int max, int since) : ThreadedCanvasManipulator(m) {
         maxDays = max;
         daysSince = since;
     }
+
     void Run() {
+        //DrawNumber(canvas, max, 0); // First matrix
+        //DrawNumber(canvas, since, 32); // Second matrix
     }
 
-static void DrawNumber(Canvas *canvas, int days) {
-    string Result;
-    ostringstream convert;
-    convert << days;
-    Result = convert.str();
-    int len = Result.length();
-    // Convert num to string
-    printf("You have chosen: %d\n", days);
-    printf("The length of this number: %d\n", len);
+    void DrawNumber(Canvas *canvas, int days, int offset) {
+        string Result;
+        ostringstream convert;
+        convert << days;
+        Result = convert.str();
+        int len = Result.length();
+        // Convert num to string
+        printf("You have chosen: %d\n", days);
+        printf("The length of this number: %d\n", len);
 
 
-    // Set colors based on days
-    int r,g,b = 0;
-    if (days <= 20) {
-        r = 255;
-        g = 0;
-        b = 0;
-    } else if (days < 50 && days > 20) {
-        r = 255;
-        g = 255;
-        b = 0;
-    } else if (days > 999) {
-        days = 999;
-        r = 255;
-        g = 0;
-        b = 0;
-    } else {
-        r = 0;
-        g = 255;
-        b = 0;
-    }
-
-    for (int i = 0; i < len; i++) {
-        /* Set the offset we'll use for the characters position */
-        int offset = 0;
-        switch (i) {
-            case 1:
-                offset = 11;
-                break;
-            case 2:
-                offset = 22;
-                break;
+        // Set colors based on days
+        int r,g,b = 0;
+        if (days <= 20) {
+            r = 255;
+            g = 0;
+            b = 0;
+        } else if (days < 50 && days > 20) {
+            r = 255;
+            g = 255;
+            b = 0;
+        } else if (days > 999) {
+            days = 999;
+            r = 255;
+            g = 0;
+            b = 0;
+        } else {
+            r = 0;
+            g = 255;
+            b = 0;
         }
 
-       /* Modify the offset, depending on how many characters there are.
-        * This allows them to be right justified.
-        */
-        switch (len) {
-            case 1:
-                offset += 22;
-                break;
-            case 2:
-                offset += 11;
-                break;
-        }
+        for (int i = 0; i < len; i++) {
+            /* Set the offset we'll use for the characters position */
+            switch (i) {
+                case 1:
+                    offset = 11;
+                    break;
+                case 2:
+                    offset = 22;
+                    break;
+            }
 
-        if (Result.substr(i, 1) == "0") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_ZERO[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
+           /* Modify the offset, depending on how many characters there are.
+            * This allows them to be right justified.
+            */
+            switch (len) {
+                case 1:
+                    offset += 22;
+                    break;
+                case 2:
+                    offset += 11;
+                    break;
             }
-        } else if (Result.substr(i,1) == "1") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_ONE[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
-            }
-        } else if (Result.substr(i,1) == "2") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_TWO[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
-            }
-        } else if (Result.substr(i,1) == "3") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_THREE[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
-            }
-        } else if (Result.substr(i,1) == "4") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_FOUR[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
-            }
-        } else if (Result.substr(i,1) == "5") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_FIVE[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
-            }
-        } else if (Result.substr(i,1) == "6") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_SIX[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
-            }
-        } else if (Result.substr(i,1) == "7") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_SEVEN[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
-            }
-        } else if (Result.substr(i,1) == "8") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_EIGHT[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
-            }
-        } else if (Result.substr(i,1) == "9") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_NINE[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
-                    }
-                }
-            }
-        }
-    }
-}
 
-static void DrawSecondNumber(Canvas *canvas, int days) {
-    string Result;          // string which will contain the result
-    ostringstream convert;   // stream used for the conversion
-    convert << days;      // insert the textual representation of 'Number' in the characters in the stream
-    Result = convert.str(); // set 'Result' to the contents of the stream
-    int len = Result.length();
-    // Convert num to string
-    printf("You have chosen: %d\n", days);
-    printf("The length of this number: %d\n", len);
-
-
-    // Set colors based on days
-    int r,g,b = 0;
-    if (days <= 20) {
-        r = 255;
-        g = 0;
-        b = 0;
-    } else if (days < 50 && days > 20) {
-        r = 255;
-        g = 255;
-        b = 0;
-    } else if (days > 999) {
-        days = 999;
-        r = 255;
-        g = 0;
-        b = 0;
-    } else {
-        r = 0;
-        g = 255;
-        b = 0;
-    }
-
-    for (int i = 0; i < len; i++) {
-        /* Set the offset we'll use for the characters position */
-        int offset = 32;
-        switch (i) {
-            case 1:
-                offset += 11;
-                break;
-            case 2:
-                offset += 22;
-                break;
-        }
-
-       /* Modify the offset, depending on how many characters there are.
-        * This allows them to be right justified.
-        */
-        switch (len) {
-            case 1:
-                offset += 22;
-                break;
-            case 2:
-                offset += 11;
-                break;
-        }
-
-        if (Result.substr(i, 1) == "0") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_ZERO[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            if (Result.substr(i, 1) == "0") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_ZERO[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
-            }
-        } else if (Result.substr(i,1) == "1") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_ONE[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            } else if (Result.substr(i,1) == "1") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_ONE[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
-            }
-        } else if (Result.substr(i,1) == "2") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_TWO[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            } else if (Result.substr(i,1) == "2") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_TWO[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
-            }
-        } else if (Result.substr(i,1) == "3") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_THREE[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            } else if (Result.substr(i,1) == "3") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_THREE[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
-            }
-        } else if (Result.substr(i,1) == "4") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_FOUR[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            } else if (Result.substr(i,1) == "4") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_FOUR[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
-            }
-        } else if (Result.substr(i,1) == "5") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_FIVE[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            } else if (Result.substr(i,1) == "5") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_FIVE[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
-            }
-        } else if (Result.substr(i,1) == "6") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_SIX[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            } else if (Result.substr(i,1) == "6") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_SIX[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
-            }
-        } else if (Result.substr(i,1) == "7") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_SEVEN[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            } else if (Result.substr(i,1) == "7") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_SEVEN[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
-            }
-        } else if (Result.substr(i,1) == "8") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_EIGHT[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            } else if (Result.substr(i,1) == "8") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_EIGHT[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
-            }
-        } else if (Result.substr(i,1) == "9") {
-            for ( int y = 0; y < 10; y++ ) {
-                for ( int x = 0; x < 16; x++ ) { 
-                    if (CHAR_NINE[x][y] == 1) {
-                        canvas->SetPixel(y + offset, x, r, g, b);
+            } else if (Result.substr(i,1) == "9") {
+                for ( int y = 0; y < 10; y++ ) {
+                    for ( int x = 0; x < 16; x++ ) {
+                        if (CHAR_NINE[x][y] == 1) {
+                            canvas->SetPixel(y + offset, x, r, g, b);
+                        }
                     }
                 }
             }
         }
     }
-}
-
 };
 
-
-
 int main(int argc, char *argv[]) {
-    bool debug_mode = true; 
+    bool debug_mode = false;
     int since = 0;
     int max = 0;
 
@@ -365,13 +225,12 @@ int main(int argc, char *argv[]) {
     RGBMatrix *matrix = new RGBMatrix(&io, rows, chain);
     Canvas *canvas = matrix;
 
-    ThreadedCanvasManipulator *image_gen = NumberCanvas(canvas, max, since);
+    ThreadedCanvasManipulator *image_gen = new NumberCanvas(canvas, max, since);
     image_gen->Start();
 
-    // Sleep forever!!!
-    while (true) {
-        usleep(1);
-    }
+    //Sleep forever
+    sleep(INT_MAX);
+
     // Animation finished. Shut down the RGB matrix.
     canvas->ClearScreen();
     delete image_gen;
